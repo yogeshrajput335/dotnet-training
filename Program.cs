6b2966c4c1c3c879e6341c0bdd8379802d4ce654
Yogesh Rajput <Yogesh.Rajput@meritarc.com>
2023-01-13 09:15:59 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyApp;
+
+int i=10;
+int j=20;
+Console.WriteLine("Hello, World!"+(i+j));
+Sample s = new Sample();
+s.Meth();
+
---
+++ Sample.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Sample
+    {
+        public void Meth()
+        {
+            Console.WriteLine("Hello World! Math");
+        }
+    }
+}
