ff7f4eaef7b67df5b687ceb912ecc2efa628f86f
Yogesh Rajput <Yogesh.Rajput@meritarc.com>
2023-01-20 09:20:21 +0530

    Input and outpput

--- Program.cs
+++ Program.cs
@@ -3,6 +3,18 @@ using MyApp;
 using InheritanceUniverse;
 
 
+Console.WriteLine("Enter your age:");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Your age is: " + age);
+// Type your username and press enter
+//Console.WriteLine("Enter username:");
+
+// // Create a string variable and get user input from the keyboard and store it in the variable
+// string userName = Console.ReadLine();
+
+// // Print the value of the variable (userName), which will display the input value
+// Console.WriteLine("Username is: " + userName);
+
 ClassA a = new ClassA();
 string aValue = a.value;
 Console.WriteLine(aValue);
